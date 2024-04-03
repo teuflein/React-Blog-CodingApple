6632ace6099b6611b82b0e79540ee5a5e5d3d792
Jeremy <teuflein@naver.com>
2024-04-04 00:20:31 +0900

    240404집

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 //import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  let posts = '강남 고기 맛집';
+  let [글제목,글제목변경] = useState(['남자코트 추천','남자바지 추천']);
+  let posts = "강남 맛집 추천";
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div> { posts } </div>
+      <div className='list'> 
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 27일 발행</p>
+        <br/>
+      </div>
     </div>
   );
 }
